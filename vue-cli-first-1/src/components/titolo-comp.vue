b1aceb483a0503c3d1790f870a92c3215a09260a
dinoarpa98 <dinoarpa98@gmail.com>
2022-04-11 15:22:15 +0200

    end1

---
+++ vue-cli-first-1/src/components/titolo-comp.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>La mia prima app con Vue CLI!</h2>
+</template>
+
+<script>
+export default {
+  name: 'titolo-comp',
+  
+}
+</script>
+
+<style>
+ h2{
+     color: rgb(146, 50, 50);
+ }
+
+</style>
